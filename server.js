007507621bfe0f95627505170ead571fa73761a5
Winston Malcolm <101194070+winstonmalcolm-tech@users.noreply.github.com>
2024-03-27 21:01:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 const io = new Server(server, {
     cors: {
-        origin: "https://justpost2.000webhostapp.com",
+        origin: "http://justpost.great-site.net",
         methods: ['GET', 'POST']
     }
 });
